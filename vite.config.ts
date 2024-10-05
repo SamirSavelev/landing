6aa85de3f8e0db6bab9b93c8242121251cffbc8c
Michael Savelev <michaeloriginal@yandex.ru>
2024-10-05 16:56:15 +0300

    Fix

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@app': path.resolve(__dirname, 'src/app'),
+      '@entities': path.resolve(__dirname, 'src/entities'),
+      '@features': path.resolve(__dirname, 'src/features'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@shared': path.resolve(__dirname, 'src/shared'),
+      '@process': path.resolve(__dirname, 'src/process'),
+      '@theme': path.resolve(__dirname, 'src/theme'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+    },
+  },
+});
